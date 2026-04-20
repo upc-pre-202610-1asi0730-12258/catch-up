464775ef831c6162c49684e4d7f20f1b9ab26955
Angel Velasquez <pcisavel@upc.edu.pe>
2026-04-20 18:06:29 -0500

    feat(layout): create layout component with language switcher and footer integration.

--- src/app.vue
+++ src/app.vue
@@ -1,7 +1,7 @@
 <script setup>
-
+import Layout from "./shared/presentation/components/layout.vue";
 </script>
 
 <template>
-
+  <layout></layout>
 </template>
---
+++ src/shared/presentation/components/layout.vue
@@ -0,0 +1,34 @@
+<script setup>
+import {ref} from "vue";
+import LanguageSwitcher from "./language-switcher.vue";
+import FooterContent from "./footer-content.vue";
+
+const drawerVisible = ref(false);
+const toggleDrawer = () => {
+  drawerVisible.value = !drawerVisible.value
+};
+</script>
+
+<template>
+  <div>
+    <div>
+      <pv-menubar>
+        <template #start>
+          <pv-button icon="pi pi-bars" label="CatchUp" text @click="toggleDrawer"/>
+          <!-- TODO: Add source list -->
+        </template>
+        <template #end>
+          <language-switcher></language-switcher>
+        </template>
+      </pv-menubar>
+    </div>
+  </div>
+  <div>
+    <!-- TODO: Add article list -->
+  </div>
+  <footer-content></footer-content>
+</template>
+
+<style scoped>
+
+</style>
