24648e933a94f6a45fa84a61111629d3150877b9
Angel Velasquez <pcisavel@upc.edu.pe>
2026-04-20 17:13:58 -0500

    feat(material-design-ui): integrate prime-vue components and configure app.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './app.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './app.vue';
+import PrimeVue from 'primevue/config';
+import Material from '@primeuix/themes/material';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+import {Avatar, Button, Card, Drawer,
+    Menu, Menubar, SelectButton, Toolbar, Tooltip} from "primevue";
 
-createApp(App).mount('#app')
+
+createApp(App)
+    .use(PrimeVue, { ripple: true, theme: { preset: Material }})
+    .component('pv-button', Button)
+    .component('pv-select-button', SelectButton)
+    .component('pv-avatar', Avatar)
+    .component('pv-drawer', Drawer)
+    .component('pv-card', Card)
+    .component('pv-image', Image)
+    .component('pv-toolbar', Toolbar)
+    .component('pv-menu', Menu)
+    .component('pv-menubar', Menubar)
+    .directive('tooltip', Tooltip)
+    .mount('#app')
