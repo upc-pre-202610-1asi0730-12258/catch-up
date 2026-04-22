edc85a931578c1b652af2142f2fe9b145727f1bb
Angel Velasquez <pcisavel@upc.edu.pe>
2026-04-22 13:13:30 -0500

    feat(layout): integrate source selection and article display components.

--- src/shared/presentation/components/layout.vue
+++ src/shared/presentation/components/layout.vue
@@ -1,12 +1,40 @@
 <script setup>
-import {ref} from "vue";
+import {computed, onMounted, ref} from "vue";
 import LanguageSwitcher from "./language-switcher.vue";
 import FooterContent from "./footer-content.vue";
+import {newsStore} from "../../../news/application/news.store.js";
+import ArticleList from "../../../news/presentation/components/article-list.vue";
+import UnavailableContent from "../../../news/presentation/components/unavailable-content.vue";
+import SourceList from "../../../news/presentation/components/source-list.vue";
 
 const drawerVisible = ref(false);
 const toggleDrawer = () => {
   drawerVisible.value = !drawerVisible.value
 };
+
+const sources = computed(() => newsStore.sources);
+const errors = computed(() => newsStore.errors);
+let articles = computed(() => newsStore.articles);
+const rerenderKey = ref(0);
+
+const setSource = source => {
+  console.log(source);
+  newsStore.setCurrentSource(source);
+  articles = computed(() => newsStore.articles);
+  rerenderKey.value += 1; // Force re-render of ArticleList
+  toggleDrawer();
+};
+
+onMounted(() => {
+  console.log('Entering onMounted');
+  newsStore.loadSources();
+  console.log(sources.value);
+  console.log(sources.value.length);
+  rerenderKey.value += 1; // Force re-render of ArticleList
+  console.log('Quiting onMounted');
+});
+
+
 </script>
 
 <template>
@@ -15,7 +43,9 @@ const toggleDrawer = () => {
       <pv-menubar>
         <template #start>
           <pv-button icon="pi pi-bars" label="CatchUp" text @click="toggleDrawer"/>
-          <!-- TODO: Add source list -->
+          <source-list v-model:sources="sources"
+                       v-model:visible="drawerVisible"
+                       v-on:source-selected="setSource($event)"/>
         </template>
         <template #end>
           <language-switcher></language-switcher>
@@ -24,7 +54,8 @@ const toggleDrawer = () => {
     </div>
   </div>
   <div>
-    <!-- TODO: Add article list -->
+    <article-list v-if="articles" v-model:articles="articles" :key="rerenderKey"/>
+    <unavailable-content v-else :errors="errors"></unavailable-content>
   </div>
   <footer-content></footer-content>
 </template>
